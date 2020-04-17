683f16a8692163afaa86e39c6e1e032b9feb439f
Juan Pablo Saladino <juanpablosaladino@gmail.com>
2020-04-17 10:59:08 -0300

    acordeon en jquery

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    var icon = {
+        header: "ui-icon-triangle-1-e",
+        activeHeader: "ui-icon-triangle-1-s"
+    };
+
+
+    $('#accordion').accordion({
+        icons : icon
+    });
+
+    
+
+
+});
